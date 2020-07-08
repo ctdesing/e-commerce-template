e8054d75d9c2bebd063daadb6b104ee0e4d8692d
Jhon Nieves <jhonnieves@jhons-air.home>
2020-07-08 16:47:29 -0400

    welocme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/welcome_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,6 +7,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      initialRoute: WelcomeScreen,
+      routes: {
+        WelcomeScreen: (context) => WelcomeScreen(),
+        // LoginScreen.id: (context) => LoginScreen(),
+        // ProductScreen.id: (context) => ProductScreen(),
+        // ProductList.id: (context) => ProductList(),
+        // RegistrationScreen.id: (context) => RegistrationScreen(),
+        // CheckoutScreen.id: (context) => CheckoutScreen(),
+      },
+    );
   }
 }
