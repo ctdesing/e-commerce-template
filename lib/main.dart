bfe039fefa9b5e59632db7ea17e9673249d67f26
Miguel Arias <ctdesing@gmail.com>
2020-07-08 17:36:02 -0400

    assets

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Container(
+        color: Colors.blue,
+      ),
+    );
   }
 }
